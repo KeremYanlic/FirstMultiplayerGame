32ef1d85eab68d1b7e1be814268a7fbdf188cd1b
KeremYanlic <keremyanlc@outlook.com>
2023-12-07 12:38:21 +0300

    Respawning Coin script file has been added and updated

---
+++ Scripts/Game/Coin/RespawningCoin.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RespawningCoin : Coin
+{
+    public event Action<RespawningCoin> OnCoinCollected;
+    private Vector3 previousPosition;
+
+
+    private void Update()
+    {
+        //if(previousPosition != transform.position)
+        //{
+        //    Show(false);
+        //}
+        //previousPosition = transform.position;
+    }
+    public override int Collect()
+    {
+        if (!IsServer)
+        {
+            Show(false);
+            return 0;
+        }
+        if (alreadyCollected) return 0;
+
+        alreadyCollected = true;
+        OnCoinCollected?.Invoke(this);
+
+        return coinValue;
+    }
+    public void Reset()
+    {
+        alreadyCollected = false;
+    }
+}
