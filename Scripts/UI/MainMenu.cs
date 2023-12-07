6cbe163c823512ac40a44a0662af24509ac69a8f
KeremYanlic <keremyanlc@outlook.com>
2023-12-07 12:51:19 +0300

    MainMenu

---
+++ Scripts/UI/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField joinCodeField;
+   
+
+    public async void StartHost()
+    {
+        await HostSingleton.Instance.hostGameManager.StartHostAsync();
+    }
+    public async void StartClient()
+    {
+        await ClientSingleton.Instance.ClientGameManager.StartClientAsync(joinCodeField.text);
+    }
+}
